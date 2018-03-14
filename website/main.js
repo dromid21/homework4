1fc3cddd1dee350217695cb0e3452bf38e175080
dromid21 <dromid21@gmail.com>
2018-03-13 20:10:32 -0700

    Fetch updated and improved. Need to alphabetize.

--- website/main.js
+++ website/main.js
@@ -1,3 +1,28 @@
-const url = 'https://jsonplaceholder.typicode.com/users'
+const url = `https://jsonplaceholder.typicode.com/users`;
 
-console.log(JSON.stringify(url.main.username));
+// Fetch procedure
+
+fetch(url) // Fetch url constant
+.then(res => res.json())
+.then((extract) => {
+    
+    // Initialize empty usernames array for later
+    let usernames = []; 
+
+    // Begin function iterations using forEach
+    extract.forEach(function(unameExtract){ // Declaration of username extract function
+        
+        // Extract username attribute from JSON file and push to usernames array, adding
+        // space and quote strings for looks as specified in instructions
+        usernames.push(` ` + `"` + unameExtract.username + `"`)
+        
+        // Determine alphabetic order using sort method on string (Thanks textbook, lol)
+        usernames.sort(function(a, b){
+            return a.length - b.length;
+        });
+        
+        // Plug in usernames array into HTML file via 'fet' tag. Add in brackets for desired look.
+        document.getElementById(`fet`).innerHTML = 
+        'User usernames from fetch: </br></br>' + '[' + usernames + ']';
+    });
+});
